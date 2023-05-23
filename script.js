27b8d7c576cbc9478c782d78a0a31910fd7ad47c
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-23 19:35:25 -0300

    setimo requisito completo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('texto-tarefa');
 const addBtn = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
+const li = document.querySelectorAll('li');
 
 const addList = () => {
   const list = document.createElement('li');
@@ -9,3 +10,7 @@ const addList = () => {
   input.value = '';
 };
 addBtn.addEventListener('click', addList);
+
+ol.addEventListener('click', (event) => {
+  event.target.style.backgroundColor = 'gray';
+});
